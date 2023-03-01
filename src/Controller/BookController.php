3d709545d3d270352c966f612edbf06b63d513d4
Marc <marcgaya98@gmail.com>
2023-03-01 11:47:59 +0100

    hola

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -1,68 +1,87 @@
 <?php
+
 namespace App\Controller;
 
 use App\Entity\Book;
 use App\Repository\BooksRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Json;
 use Symfony\Config\Framework\RequestConfig;
 
 class BookController extends AbstractController
 {
-    #[Route('/book', name: 'list_book', methods: ['GET'])]
-    public function list(BooksRepository $repo): Response
+    #[Route('/ws/books-before-year/{year}', name: 'books-before-year')]
+    public function booksBeforeYear(BooksRepository $repo, string $year): JsonResponse
     {
-        return $this->render('book/list.html.twig', [
-            'books' => $repo->findAll()
+        $books = [];
+        foreach ($repo->findAllBeforeYear($year) as $book) {
+            $books[] = $book->toArray();
+        }
+        return new JsonResponse([
+            'books' => $books
         ]);
     }
 
-    #[Route('/book/{slug}', name: 'show_book', methods: ['GET'])]
-    public function show(string $slug, BooksRepository $repo): Response
+    /*
+        #[Route('/book', name: 'list_book', methods: ['GET'])]
+        public function list(BooksRepository $repo): Response
+        {
+            return $this->render('book/list.html.twig', [
+                'books' => $repo->findAll()
+            ]);
+        }
+    */
+    #[Route('/ws/book/{slug}', name: 'show_book', methods: ['GET'])]
+    public function show(string $slug, BooksRepository $repo): JsonResponse
     {
+        $bk = $repo->findOneBy(['isbn' => $slug]);
+
+        return new JsonResponse($bk->toArray());
+
+        /*
         return $this->render('book/show.html.twig', [
             'book' => $repo->findOneBy(
                 ['isbn' => $slug]
             )
-        ]);
+        ]);*/
     }
+    /*
+        #[Route('/book', name: 'book_list_c:', methods: ['GET'])]
+        public function category(string $slug): Response
+        {
+            return $this->render('book/list.html.twig', [
 
-    #[Route('/book', name: 'book_list_y:', methods: ['GET'])]
-    public function year(string $year, BooksRepository $repo): Response
+            ]);
+        }
+*/
+    #[Route('ws/add-book', name: 'add-book:')]
+    public function addBook(ManagerRegistry $doctrine, BooksRepository $repo, string $query): Response
     {
-        return $this->render('book/list.html.twig', [
-            'books' => $repo->findAllBefore2013(
-                ['date' => $year]
-            )
-        ]);
-    }
+        $entityManager = $doctrine->getManager();
 
-    #[Route('/book', name: 'book_list_c:', methods: ['GET'])]
-    public function category(string $slug): Response
-    {
-        return $this->render('book/list.html.twig', [
-
-        ]);
-    }
-
-    #[Route('/book', name: 'book_add:', methods: ['POST'])]
-    public function new(Request $request): Response
-    {
         $book = new Book();
+
+        $book->setFromJson($query);
+
+        $repo->save($book);
+
 
         return $this->render('book/show.html.twig', [
 
         ]);
     }
+/*
+        #[Route('/book/{slug}', name: 'book_remove:', methods: ['DELETE'])]
+        public function remove(string $slug): Response
+        {
+            return $this->render('book/list.html.twig', [
 
-    #[Route('/book/{slug}', name: 'book_remove:', methods: ['DELETE'])]
-    public function remove(string $slug): Response
-    {
-        return $this->render('book/list.html.twig', [
-
-        ]);
-    }
+            ]);
+        }*/
 
 
 
